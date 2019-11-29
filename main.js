eccc373d847bc9b9de33b226e732cc60b8412bf8
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-11-29 16:22:43 +0100

    Bonus Completato, piccole modifiche stilistiche per il responsive

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-// Creare in HTML una griglia formata da 8x8 quadratini tutti bianchi.
-// 15 di questi quadratini (scelti a caso all’inizio) se cliccati diventano rossi, gli altri diventano verdi
-// Suggerimento: usare una classe per individuare quali quadratini diventano rossi
 // BONUS: generare la griglia dei quadratini utilizzando jQuery
-// Nome repo: js-jq-grigliaquadrati
 
+// ciclo while che crea 8 row
+while (($('.row').length) < 8) {
+    $('.squareyard').append('<div class="row"></div>');
+};
+// ciclo for per creare, all'interno di ogni row, 8 square,
+for (var i = 0; i < 8; i++) {
+    $('.row').append('<div class="square"></div>');
+};
+
+// il log finale dovrebbe portare 8 row e 64 square
+console.log($('.row').length);
+console.log($('.square').length);
 
 
 // creo un array vuota di supporto, per far si che i numeri non si ripetano
@@ -16,7 +24,7 @@ while(ranArray.length < 15) {
     if (!ranArray.includes(num)) {
         // ne pusha uno nell'array
         ranArray.push(num);
-        // e con lo stesso numero ne prende uno in poszione casuale tra quelli dentro l'html
+        // e con lo stesso numero ne prende uno in poszione 'num-random' tra quelli dentro l'html
         // e gli assegna la classe danger
         $('.squareyard').find('.square').eq(num).addClass('danger');
     }
@@ -24,8 +32,7 @@ while(ranArray.length < 15) {
 // faccio un log momentano per assicurarmi che l'Array abbia 15 numeri diversi tra loro,
 // per comodità li metto in ordine crescente, si legge meglio
 console.log(myNumbSorting(ranArray));
-console.log($('.square').length);
-console.log($('.row').length);
+
 // con la stessa logica, continuo a riempire l'Array con i restanti numeri;
 // solo che questa volta assegna classe sure;
 // alla fine del ciclo dovrei ottenere un array con tutti i numeri da 0 a 63, che
